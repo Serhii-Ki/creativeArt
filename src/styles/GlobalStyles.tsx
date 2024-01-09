e5afcde6a22b86d7e95b8d0e2e5eea8582225fa6
Sergey Kiba <serhiikiba@gmail.com>
2024-01-09 21:32:17 +0100

    Added global styles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,5 +1,42 @@
-function GlobalStyles() {
-	return <div></div>;
+import { createGlobalStyle } from 'styled-components';
+
+import bg from '../assets/bg.png';
+
+const GlobalStyles = createGlobalStyle`
+	*,
+	*::before,
+	*::after {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body {
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    color: #FFFFFD;
+		flex-shrink: 0;
+    background: url(${bg}) lightgray 0px -0.827px / 179.034% 100.035% no-repeat;
+    mix-blend-mode: color-burn;
 }
+
+	a {
+		font-size: 20px;
+		font-weight: 500;
+		text-decoration: none;
+		color: rgba(255, 255, 253, 0.72);
+	}
+
+	a:hover {
+		color: #FFF;
+		text-decoration: underline #D3F85A;
+	}
+
+	ul,
+	li {
+		list-style: none;
+	}
+`;
 
 export default GlobalStyles;
